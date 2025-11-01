5a3874e6157bfc7364620fb0b7dd08a4cf3a8742
Nakamura25257 <k.eagle160@gmail.com>
2025-11-02 01:14:51 +0900

    creating environment

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # calendar-share
+Creating a Web calendar app to share holidays with my friends.
---
+++ calendar-share/pages/index.tsx
@@ -0,0 +1,8 @@
+import styles from './styles/index.module.css'
+
+export default function IndexPage() {
+  return(
+    <div className={styles.main}>      
+    </div>
+  )
+}
