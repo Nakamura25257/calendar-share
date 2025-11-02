37f70b42366626f0478679c55ac925a6a59278f2
Nakamura25257 <k.eagle160@gmail.com>
2025-11-02 13:05:45 +0900

    feat: create login/register page

---
+++ calendar-share/pages/register.tsx
@@ -0,0 +1,75 @@
+import Link from 'next/link';
+import styles from './styles/auth.module.css';
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {registerSchema, RegisterSchemaType} from '@/types/resolver';
+
+export default function RegisterPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({resolver: zodResolver(registerSchema)});
+
+  const onSubmit = (data: RegisterSchemaType) => {
+    console.log('data', data);
+  };
+
+  return (
+    <div className={styles.registerWrapper}>
+      <h1>新規登録</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.inputWrapper}>
+          <label>名前</label>
+          <input type="text" placeholder="山田太郎" {...register('name')} />
+        </div>
+        {errors.name && (
+          <p className={styles.errorMessage}>{errors.name.message}</p>
+        )}
+
+        <div className={styles.inputWrapper}>
+          <label>メールアドレス</label>
+          <input
+            type="text"
+            placeholder="example@email.com"
+            {...register('email')}
+          />
+        </div>
+        {errors.email && (
+          <p className={styles.errorMessage}>{errors.email.message}</p>
+        )}
+
+        <div className={styles.inputWrapper}>
+          <label>パスワード</label>
+          <input
+            type="password"
+            placeholder="********"
+            {...register('password')}
+          />
+        </div>
+        {errors.password && (
+          <p className={styles.errorMessage}>{errors.password.message}</p>
+        )}
+
+        <div className={styles.inputWrapper}>
+          <label>パスワード（確認）</label>
+          <input
+            type="password"
+            placeholder="********"
+            {...register('confirmPassword')}
+          />
+        </div>
+        {errors.confirmPassword && (
+          <p className={styles.errorMessage}>
+            {errors.confirmPassword.message}
+          </p>
+        )}
+
+        <button type="submit">登録</button>
+      </form>
+      <Link href="/login">
+        <span>アカウントをお持ちでない方はこちら</span>
+      </Link>
+    </div>
+  );
+}
---
+++ calendar-share/types/resolver.ts
@@ -0,0 +1,29 @@
+import z from 'zod';
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, {message: '名前を入力してください'})
+    .regex(
+      /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+      {message: 'メールアドレスの形式が正しくありません'},
+    ),
+  password: z.string().min(1, {message: 'パスワードを入力してください'}),
+});
+
+export const registerSchema = z
+  .object({
+    name: z.string().min(1, {message: '名前を入力してください'}),
+    email: z.string().min(1, {message: 'メールアドレスを入力してください'}),
+    password: z.string().min(1, {message: 'パスワードを入力してください'}),
+    confirmPassword: z
+      .string()
+      .min(1, {message: '確認用パスワードを入力してください'}),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    message: 'パスワードが一致しません',
+    path: ['passwordConfirmation'],
+  });
+
+export type LoginShcemaType = z.infer<typeof loginSchema>;
+export type RegisterSchemaType = z.infer<typeof registerSchema>;
